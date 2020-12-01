61661c9ab9bf66adf035ef09fa8b116db4b3109a
mohammed <mohamedwork636@gmail.com>
2020-12-01 12:03:04 +0200

    create super admin and register admin models

--- handworkers/handworker/admin.py
+++ handworkers/handworker/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, City, Workers, Home, RateWorkers, Phone, Governorate
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(City)
+admin.site.register(Workers)
+admin.site.register(Home)
+admin.site.register(RateWorkers)
+admin.site.register(Phone)
+admin.site.register(Governorate)
+
+
